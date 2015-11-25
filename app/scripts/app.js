8715b96d4a5b89f6e7849217f85ddd3ee4c37cfe
Andrew Stuart <andrew.stuart2@gmail.com>
2015-11-24 19:43:58 -0700

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name multi2App
+ * @description
+ * # multi2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('multi2App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
